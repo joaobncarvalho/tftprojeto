5c9414cd48cae173980a88d089e0a33b66d2a3d5
João Carvalho <joaove>
2022-12-24 01:53:37 +0000

    make it better

--- tftprojeto/models/connection.js
+++ tftprojeto/models/connection.js
@@ -1,6 +1,6 @@
 var pg = require('pg'); 
  
-const connectionString = "postgres://postgres:tftprojeto@localhost:5432/tftdatabasedefinitivo"  //Conectar com os dados normais da BD postgresql
+const connectionString = "postgres://eumvhaqbuiocfn:ee538131a5a6342be636862cc7343237804431909cb1604d535243ce8c0e5600@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d1fahila4mudhm"  //Conectar com os dados normais da BD postgresql
 const Pool = pg.Pool 
 /*const pool = new Pool({ 
     connectionString, 
@@ -12,7 +12,7 @@ const Pool = pg.Pool
 }) */
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || 'postgres://postgres:tftprojeto@localhost:5432/tftdatabasedefinitivo',
+    connectionString: process.env.DATABASE_URL || 'postgres://eumvhaqbuiocfn:ee538131a5a6342be636862cc7343237804431909cb1604d535243ce8c0e5600@ec2-3-229-161-70.compute-1.amazonaws.com:5432/d1fahila4mudhm',
     ssl: process.env.DATABASE_URL ? true : false
 })
  
